44c8ac758e03782bc2eeaedd4bfe10d867f18d2f
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-05-07 13:30:03 +0100

    cleanup

--- src/ver1/Checkout.ts
+++ src/ver1/Checkout.ts
@@ -2,12 +2,6 @@ import AbstractCheckout from "../interfaces/AbstractCheckout.ts";
 
 import Ver1Rule from "./Ver1Rule.ts";
 
-/**
- * I've introduced separate Checkout classes for in memory and in database
- * implementations to introduce point in code where we can create new Checkout
- * implementations by just injecting different types but if internally these should be
- * handled differently then this is the level where we can override methods from AbstractCheckout
- */
 export default class Checkout extends AbstractCheckout<string, number, Ver1Rule> {
   protected rules: Ver1Rule[];
   constructor(rules: Ver1Rule[]) {
--- src/ver2/Checkout.ts
+++ src/ver2/Checkout.ts
@@ -6,12 +6,6 @@ import AbstractCheckout from "../interfaces/AbstractCheckout.ts";
 
 type RuleType = typeof schema.rules.$inferSelect;
 
-/**
- * I've introduced separate Checkout classes for in memory and in database
- * implementations to introduce point in code where we can create new Checkout
- * implementations by just injecting different types but if internally these should be
- * handled differently then this is the level where we can override methods from AbstractCheckout
- */
 export default class Checkout extends AbstractCheckout<string, number, RuleType> {
   async findRules(uniqueItems: string[]): Promise<RuleType[]> {
     return await db.select().from(schema.rules).where(inArray(schema.rules.sku, uniqueItems));
